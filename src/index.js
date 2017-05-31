7551025ac3982637aa1efd737084d212a6960039
littlewin <littlewin.wang@gmail.com>
2017-05-31 17:09:53 +0800

    update basic routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import 'core-js/fn/object/assign'
-import React from 'react'
 import ReactDOM from 'react-dom'
 
 // import { Provider } from 'react-redux'
-// import routes from './routes'
+import routes from './routes'
 
 import 'normalize.css'
 import 'styles/index.styl'
 
-import NodeGraphContainer from 'containers/NodeGraphContainer'
-
 // Render the main component into the dom
 ReactDOM.render(
-  <NodeGraphContainer />,
+  routes,
   document.getElementById('app')
 )
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
-import SearchContainer from 'components/containers/SearchContainer'
-import NodeGraphContainer from 'components/containers/NodeGraphContainer'
+import SearchContainer from 'containers/SearchContainer'
+import NodeGraphContainer from 'containers/NodeGraphContainer'
 
 const route = (
   <Router history={browserHistory}>
-    <Route path='/' component={SearchContainer}>
+    <Route path='/'>
+      <IndexRoute component={SearchContainer} />
       <Route path='artist/:id' component={NodeGraphContainer} />
     </Route>
   </Router>
