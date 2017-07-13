6bd5336a42cbe527c497820553322e4cb1e1c5b7
littlewin <littlewin.wang@gmail.com>
2017-07-13 23:00:24 +0800

    update component modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Squares from 'react-activity/lib/Squares'
+import Sentry from 'react-activity/lib/Sentry'
 
 import './Modal.styl'
 
@@ -7,7 +7,7 @@ export default class GithubLink extends React.Component {
   render () {
     return (
       <div className="modal">
-        <Squares className="modal-process" color="#727981" size={48} speed={1} />
+        <Sentry className="modal-process" color="#57caf4" size={32} speed={1} />
       </div>
     )
   }
